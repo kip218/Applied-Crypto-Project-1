25071a06ca9f31515ab631df6e5d426f7c57850c
Kang In Park <kip218@nyu.edu>
2022-10-09 18:44:43 -0400

    update

--- enc.py
+++ enc.py
@@ -1,27 +1,29 @@
 import random
 import string
 
-
-#test1
+#get test1 plaintext
 with open('test1', 'r') as file:
 	content = file.read()
 	plaintexts = content.split('\n')
-
 plaintexts = [
 				plaintexts[4], plaintexts[8],
 			  	plaintexts[12], plaintexts[16],
 			  	plaintexts[20]
 			 ]
 
+
 #Key & Text space
 key_space = [i for i in range(27)]
 text_space = [' '] + list(string.ascii_lowercase)
+
 
 #Change these values for testing
 KEY = [1, 3, 8, 10, 14, 16, 19, 21, 23] # t=9
 PLAINTEXT = plaintexts[0]
 PROB_RANDOM = 0.05
 
+
+#Encrypting
 i = 0
 ciphertext = ''
 while i < len(PLAINTEXT):
